56003438a52799c8fcf963525dfd3df0e87340b2
juan oliva <olivajuan12@gmail.com>
2022-05-17 13:19:05 -0300

    updated addresses for MATIC

--- scripts/completeSubgraph.ts
+++ scripts/completeSubgraph.ts
@@ -11,23 +11,23 @@ enum Network {
 
 const startBlockByNetwork: Record<Network, Record<string, number>> = {
   [Network.MATIC]: {
-    default: 26621371,  
+    default: 26285118,  
     // Can define contract specific also.
   },
   [Network.MUMBAI]: {
-    default: 26203727,
+    default: 26285118,
     // Can define contract specific also.
   }
 }
 
 const contractAddressByNetwork: Record<Network, Record<string, string>> = {
   [Network.MATIC]: {  // TODO: Set prod addresss here
-    admin: "0x2bae69cfb272518254335AbceAD612a09CFa9D99",
-    event: "0x05f113930E8e571D899AB6aa4484f10c92Dd5a7F",
-    ticket: 	"0xA9720bF99CB770fBadEc586b1f8D90738a268Bed",
-    ticketsMarketplace: "0x00dFE99c400dbd785dD90e94Ce426508d22c982d",
-    membership: 	"0x8Ca7a5912207a772Eb0b27A1d6df5f0B136e2e9f",
-    membershipsMarketplace: "0xEFfe806CAD8Aa7dBD3cA8554F37D4d87f148E022",
+    admin: "0xf4EfD08600Bbe35C187252FBb1eCD2F7a264D162",
+    event: "0xfc1F8ccDc5897983e56058548ff591F0F333eE9B",
+    ticket: 	"0x8f0553207e486D2C6f0636bf8f67D4D76EB84656",
+    ticketsMarketplace: "0x4f506bdE33C7B0C55694642c8b123aC85913C5E7",
+    membership: 	"0x64bC7bB5A73563657bE31e832aB1937617cEAA1D",
+    membershipsMarketplace: "0xAa263Fc559871bD019d01cae1Cc9C0FDB6d7ec5d",
   },
   [Network.MUMBAI]: {
     admin: "0x2bae69cfb272518254335AbceAD612a09CFa9D99",

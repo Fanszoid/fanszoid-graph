237ea55eb9efdcf0426c02929f44c19f771f8065
juan oliva <olivajuan12@gmail.com>
2022-12-14 08:52:33 -0300

    fix: handle parsing of incorrect json on metadata

--- src/handlers/utils.ts
+++ src/handlers/utils.ts
@@ -3,103 +3,109 @@ import { bigIntEventAttrs } from "../modules/Event";
 import { Allowance, SocialNetwork } from "../../build/generated/schema";
 
 export function parseMetadata(uri: string, entity: Entity, attrs: string[]): boolean {
-    let uriParts = uri.split("/");
-    let hash = uriParts[uriParts.length - 1];
-    let retries = 3;
-    let data: Bytes | null = null;
-    while(!data && retries > 0) {
-      data = ipfs.cat(hash);
-      retries--;
-    }
-    if (!data) {
-      log.error("IPFS error: Could not parse metadata for hash {}", [hash]);
-      return false
-    };
-  
-    let jsonParsed = json.fromBytes(data);
-    let value: TypedMap<string, JSONValue>;
-    if (jsonParsed.kind == JSONValueKind.OBJECT) {
-      value = jsonParsed.toObject();
-    } else if (jsonParsed.kind == JSONValueKind.STRING) {
-      let jsonObject = json.fromString(jsonParsed.toString());
-      if (jsonObject.kind == JSONValueKind.OBJECT) {
-        value = jsonObject.toObject();
-      } else {
-        log.error("parseMetadata: Invalid metadata obj kind {}", [jsonObject.kind.toString()]);
-        return false;
-      }
-  
-    } else {
-      log.error("parseMetadata: Invalid metadata kind {}", [jsonParsed.kind.toString()]);
-      return false;
-    }
-  
-    if (value) {
-      for (let i = 0; i < attrs.length; i++) {
-        let aux = value.get(attrs[i]);
-        if (aux) {
-          if( bigIntEventAttrs.indexOf(attrs[i]) >= 0 ) {
-            let parsedToStringValue = parseJSONValueToString(aux)
-            if( parsedToStringValue == '' || !isStringAnInteger(parsedToStringValue) ) {
-              entity.setBigInt(attrs[i], BigInt.fromI32(0))
-            } else {
-              entity.setBigInt(attrs[i], BigInt.fromString(parsedToStringValue))
-            }
-          } 
-          // especial parsing for socials
-          else if( attrs[i] == "socials") {
-            if( aux.kind === JSONValueKind.ARRAY ) {
-              let socials = aux.toArray();
+  let uriParts = uri.split("/");
+  let hash = uriParts[uriParts.length - 1];
+  let retries = 3;
+  let data: Bytes | null = null;
+  while(!data && retries > 0) {
+    data = ipfs.cat(hash);
+    retries--;
+  }
+  if (!data) {
+    log.error("IPFS error: Could not parse metadata for hash {}", [hash]);
+    return false
+  };
 
-            for( let i=0; i< socials.length ; i++) {
-              let social = socials[i];
+  let jsonParsed = json.try_fromBytes(data);
 
-              let name: string = '';
-              let url: string = '';
-
-              let socialValues = social.toObject().entries;
-              for( let i=0; i< socialValues.length ; i++) {
-                let socialValue = socialValues[i];
-                if( socialValue.key.toString() == "name" ){
-                  name = socialValue.value.toString();
-                } else if ( socialValue.key.toString() == "url" ){
-                  url = socialValue.value.toString();
-                }
-              }
-                
-                let socialNetwork = new SocialNetwork(entity.getString("id") + '-' + name + '-' + i.toString());
-                socialNetwork.name = name;
-                socialNetwork.url = url;
-                socialNetwork.event = entity.getString("id");
-
-                socialNetwork.save();
-              }
-            }
-          }
-          // hardcode our IPFS server.
-          else if( attrs[i] == "image") {
-            let strArr = parseJSONValueToString(aux).split("/")
-            if(strArr.length > 0 && strArr[strArr.length - 1].length > 0) {
-              entity.setString(attrs[i], "https://ipfs.fanz.events/ipfs/" + strArr[strArr.length - 1]);
-            } else {
-              entity.setString(attrs[i], parseJSONValueToString(aux));
-            }
-          }
-          else {
-            entity.setString(attrs[i], parseJSONValueToString(aux));
-          }
-        } else {
-          log.debug("Could not get attr: " + attrs[i].toString(), []);
-        }
-      }
-    } else {
-      log.error("parseMetadata: value is null, data: {}", [data.toString()]);
-      return false
-    }
-
-    return true
+  if(!jsonParsed.isOk) {
+    return false
   }
   
+  let value: TypedMap<string, JSONValue>;
+  if (jsonParsed.value.kind == JSONValueKind.OBJECT) {
+    value = jsonParsed.value.toObject();
+  } else if (jsonParsed.value.kind == JSONValueKind.STRING) {
+    let jsonObject = json.fromString(jsonParsed.value.toString());
+    
+    if (jsonObject.kind == JSONValueKind.OBJECT) {
+      value = jsonObject.toObject();
+    } else {
+      log.error("parseMetadata: Invalid metadata obj kind {}", [jsonObject.kind.toString()]);
+      return false;
+    }
+
+  } else {
+    log.error("parseMetadata: Invalid metadata kind {}", [jsonParsed.value.kind.toString()]);
+    return false;
+  }
+
+  if (value) {
+    for (let i = 0; i < attrs.length; i++) {
+      let aux = value.get(attrs[i]);
+      if (aux) {
+        if( bigIntEventAttrs.indexOf(attrs[i]) >= 0 ) {
+          let parsedToStringValue = parseJSONValueToString(aux)
+          if( parsedToStringValue == '' || !isStringAnInteger(parsedToStringValue) ) {
+            entity.setBigInt(attrs[i], BigInt.fromI32(0))
+          } else {
+            entity.setBigInt(attrs[i], BigInt.fromString(parsedToStringValue))
+          }
+        } 
+        // especial parsing for socials
+        else if( attrs[i] == "socials") {
+          if( aux.kind === JSONValueKind.ARRAY ) {
+            let socials = aux.toArray();
+
+          for( let i=0; i< socials.length ; i++) {
+            let social = socials[i];
+
+            let name: string = '';
+            let url: string = '';
+
+            let socialValues = social.toObject().entries;
+            for( let i=0; i< socialValues.length ; i++) {
+              let socialValue = socialValues[i];
+              if( socialValue.key.toString() == "name" ){
+                name = socialValue.value.toString();
+              } else if ( socialValue.key.toString() == "url" ){
+                url = socialValue.value.toString();
+              }
+            }
+              
+              let socialNetwork = new SocialNetwork(entity.getString("id") + '-' + name + '-' + i.toString());
+              socialNetwork.name = name;
+              socialNetwork.url = url;
+              socialNetwork.event = entity.getString("id");
+
+              socialNetwork.save();
+            }
+          }
+        }
+        // hardcode our IPFS server.
+        else if( attrs[i] == "image") {
+          let strArr = parseJSONValueToString(aux).split("/")
+          if(strArr.length > 0 && strArr[strArr.length - 1].length > 0) {
+            entity.setString(attrs[i], "https://ipfs.fanz.events/ipfs/" + strArr[strArr.length - 1]);
+          } else {
+            entity.setString(attrs[i], parseJSONValueToString(aux));
+          }
+        }
+        else {
+          entity.setString(attrs[i], parseJSONValueToString(aux));
+        }
+      } else {
+        log.debug("Could not get attr: " + attrs[i].toString(), []);
+      }
+    }
+  } else {
+    log.error("parseMetadata: value is null, data: {}", [data.toString()]);
+    return false
+  }
+
+  return true
+}
+
 export function parseJSONValueToString(value: JSONValue): string{
     switch (value.kind) {
       case JSONValueKind.STRING:

410ef6ef67aa40f4a28dc4f426896946ee6a36ee
Matias Heimann <matiasheimann@matiass-mbp.lan>
2023-03-22 11:09:38 -0300

    add organizer location

--- src/modules/Event/index.ts
+++ src/modules/Event/index.ts
@@ -5,7 +5,7 @@ export var eventAttrs: string[] = [
   'title', 'description', 'type', 'category', 'dclX', 'dclY', 'city', 
   'postalCode', 'address', 'socials', 'email', 'website', 'isAvailable', 'status', 
   'inStock', 'createdAt', 'updatedAt', 'image', 'startDateUTC', 'endDateUTC', 'location', 'timezone',
-  'fullAddress', 'latitude', 'longitude', 'mapUrl',
+  'fullAddress', 'latitude', 'longitude', 'mapUrl', 'organizerLocation'
 ];
 
 export var bigIntEventAttrs: string[] = [

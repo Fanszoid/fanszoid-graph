7d22dcbfcc2be7351bdb70b8888d175517863d42
DESKTOP-JB53URN\Franco <dinerfranco@gmail.com>
2023-05-23 14:08:07 -0300

    Add pixel code to graph

--- src/modules/Event/index.ts
+++ src/modules/Event/index.ts
@@ -2,25 +2,44 @@ import { Address, BigInt } from "@graphprotocol/graph-ts";
 import { Event } from "../../../build/generated/schema";
 
 export var eventAttrs: string[] = [
-  'title', 'description', 'type', 'category', 'dclX', 'dclY', 'city', 
-  'postalCode', 'address', 'socials', 'email', 'website', 'isAvailable', 'status', 
-  'inStock', 'createdAt', 'updatedAt', 'image', 'startDateUTC', 'endDateUTC', 'location', 'timezone',
-  'fullAddress', 'latitude', 'longitude', 'mapUrl', 'organizerLocation', 'locationName'
+  "title",
+  "description",
+  "type",
+  "category",
+  "dclX",
+  "dclY",
+  "city",
+  "postalCode",
+  "address",
+  "socials",
+  "email",
+  "website",
+  "isAvailable",
+  "status",
+  "inStock",
+  "createdAt",
+  "updatedAt",
+  "image",
+  "startDateUTC",
+  "endDateUTC",
+  "location",
+  "timezone",
+  "fullAddress",
+  "latitude",
+  "longitude",
+  "mapUrl",
+  "organizerLocation",
+  "locationName",
+  "pixelCode",
 ];
 
-export var bigIntEventAttrs: string[] = [
-  'startDateUTC', 'endDateUTC'
-];
+export var bigIntEventAttrs: string[] = ["startDateUTC", "endDateUTC"];
 
-export function getEventId(
-  eventIdContract: BigInt
-): string {
+export function getEventId(eventIdContract: BigInt): string {
   return "e" + eventIdContract.toHex();
 }
 
-export function loadOrCreateEvent(
-  eventIdContract: BigInt
-): Event {
+export function loadOrCreateEvent(eventIdContract: BigInt): Event {
   let eventId = getEventId(eventIdContract);
   let eventEntity = Event.load(eventId);
   if (eventEntity == null) {

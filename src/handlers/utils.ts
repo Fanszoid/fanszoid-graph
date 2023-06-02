a3d824c845ae4648522deb2e379562117ccf589e
Matias Heimann <matiasheimann@matiass-mbp.lan>
2023-06-02 16:20:28 -0300

    add ask buyer name and buyer id parse

--- src/handlers/utils.ts
+++ src/handlers/utils.ts
@@ -209,6 +209,8 @@ export function parseMetadata(
           }
         } else if (attrs[i] == "extra_requirement") {
           entity.setString("extraRequirement", parseJSONValueToString(aux));
+        } else if(attrs[i] == "askBuyerId" || attrs[i] == "askBuyerName") {
+            entity.setBoolean(attrs[i], aux.toBool())
         } else {
           entity.setString(attrs[i], parseJSONValueToString(aux));
         }

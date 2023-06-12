a96bd0c536cd09c91af4311af4e7b2d55a4fdfb7
Matias Heimann <matiasheimann@matiass-mbp.lan>
2023-06-12 17:42:49 -0300

    use extra question field

--- src/handlers/utils.ts
+++ src/handlers/utils.ts
@@ -211,6 +211,8 @@ export function parseMetadata(
           entity.setString("extraRequirement", parseJSONValueToString(aux));
         } else if(attrs[i] == "askBuyerId" || attrs[i] == "askBuyerName") {
             entity.setBoolean(attrs[i], aux.toBool())
+        } else if(attrs[i] == "extraImages") {
+          entity.set(attrs[i], new Value(ValueKind.ARRAY, aux.toArray()))
         } else {
           entity.setString(attrs[i], parseJSONValueToString(aux));
         }

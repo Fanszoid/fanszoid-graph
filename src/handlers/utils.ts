7f86c1f50d86f93470afa351978e0a32a53b4be8
Matias Heimann <matiasheimann@matiass-mbp.lan>
2023-06-12 17:54:28 -0300

    add type

--- src/handlers/utils.ts
+++ src/handlers/utils.ts
@@ -212,7 +212,7 @@ export function parseMetadata(
         } else if(attrs[i] == "askBuyerId" || attrs[i] == "askBuyerName") {
             entity.setBoolean(attrs[i], aux.toBool())
         } else if(attrs[i] == "extraImages") {
-          entity.set(attrs[i], Value.fromStringArray(aux.toArray().map(value => parseJSONValueToString(value))))
+          entity.set(attrs[i], Value.fromStringArray(aux.toArray().map((value: JSONValue) => parseJSONValueToString(value))))
         } else {
           entity.setString(attrs[i], parseJSONValueToString(aux));
         }
